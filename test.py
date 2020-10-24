1a5ebb0e3e21f2f5ffc7a577161369e865956073
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 20:34:06 +0530

    what is in event.json?

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import sys
 import os
 
+print(os.getenv("GITHUB_REF"))
+print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
 print(os.getenv("GITHUB_EVENT_PATH"))
-print(os.getenv("GITHUB_TOKEN")[:5])
+
+print(os.getenv("GITHUB_TOKEN"))
 #  print(os.getenv())
 #  print(os.getenv())
 #  print(os.getenv())
