6174106e1a5bb5c3ec89138148940cf8def9b00e
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 20:26:17 +0530

    Update test.yml and add test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+import sys
+import os
+
+print(os.getenv("GITHUB_EVENT_PATH"))
+#  print(os.getenv())
+#  print(os.getenv())
+#  print(os.getenv())
+#  print(os.getenv())
+#  print(os.getenv())
+#  print(os.getenv())
+
+
+
+sys.exit(1)
