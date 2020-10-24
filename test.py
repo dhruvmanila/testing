f8de912c0b5945870360881c92cf3f038fc7a1a8
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 20:56:20 +0530

    few more tests

--- test.py
+++ test.py
@@ -1,18 +1,19 @@
 import sys
 import os
+import json
+from pprint import pprint
 
 print(os.getenv("GITHUB_REF"))
 print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
 print(os.getenv("GITHUB_EVENT_PATH"))
 
-print(os.getenv("secrets.GITHUB_TOKEN"))
-#  print(os.getenv())
-#  print(os.getenv())
-#  print(os.getenv())
-#  print(os.getenv())
-#  print(os.getenv())
-#  print(os.getenv())
+json_obj = json.load(os.getenv("GITHUB_EVENT_PATH"))
+
+pprint(json_obj, indent=4)
 
 
+print("Pull Request from event.json:", json_obj["number"])
+
+print("Secret github token:", os.getenv("secrets.GITHUB_TOKEN"))
 
 sys.exit(1)
