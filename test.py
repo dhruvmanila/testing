a8ea75f21fd6db61638d34202329dcaef82ded03
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 20:58:10 +0530

    open file for JSON to load

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ print(os.getenv("GITHUB_REF"))
 print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
 print(os.getenv("GITHUB_EVENT_PATH"))
 
-json_obj = json.load(os.getenv("GITHUB_EVENT_PATH"))
+with open(os.getenv("GITHUB_EVENT_PATH")) as fp:
+    json_obj = json.load(fp)
 
 pprint(json_obj, indent=4)
 
