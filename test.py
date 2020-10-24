6b5cfdde115f8dc54edc267e5ce2acd45f7c71c4
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 21:04:18 +0530

    another try for token

--- test.py
+++ test.py
@@ -10,11 +10,11 @@ print(os.getenv("GITHUB_EVENT_PATH"))
 with open(os.getenv("GITHUB_EVENT_PATH")) as fp:
     json_obj = json.load(fp)
 
-pprint(json_obj, indent=4)
+#  pprint(json_obj, indent=4)
 
 
 print("Pull Request from event.json:", json_obj["number"])
 
-print("Secret github token:", os.getenv("secrets.GITHUB_TOKEN"))
+print("Secret github token:", os.getenv("GITHUB_TOKEN"))
 
 sys.exit(1)
