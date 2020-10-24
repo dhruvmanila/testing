cdf3f651b40ea37d68b2d8ad55d0646dea03370d
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 21:55:00 +0530

    remove failure

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
-import sys
 import os
 import json
-from pprint import pprint
 
 print(os.getenv("GITHUB_REF"))
 print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
@@ -10,11 +8,7 @@ print(os.getenv("GITHUB_EVENT_PATH"))
 with open(os.getenv("GITHUB_EVENT_PATH")) as fp:
     json_obj = json.load(fp)
 
-#  pprint(json_obj, indent=4)
-
 
 print("Pull Request from event.json:", json_obj["number"])
 
 print("Secret github token:", os.getenv("GITHUB_TOKEN")[:5])
-
-sys.exit(1)
