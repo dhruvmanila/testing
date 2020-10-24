4e996b2e2d86e7f4730e39510a734451d80a1bfa
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-24 20:50:19 +0530

    what is secret token

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print(os.getenv("GITHUB_REF"))
 print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
 print(os.getenv("GITHUB_EVENT_PATH"))
 
-print(os.getenv("GITHUB_TOKEN"))
+print(os.getenv("secrets.GITHUB_TOKEN"))
 #  print(os.getenv())
 #  print(os.getenv())
 #  print(os.getenv())
