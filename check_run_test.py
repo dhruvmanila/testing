2369014287586706a8e7d2d9bbfb5d7faf65a683
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-10-26 15:36:13 +0530

    Update check_run_test.py

--- check_run_test.py
+++ check_run_test.py
@@ -1,14 +1,3 @@
 import sys
-import os
-import json
 
-print(os.getenv("GITHUB_REF"))
-print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
-print(os.getenv("GITHUB_EVENT_PATH"))
-
-with open(os.getenv("GITHUB_EVENT_PATH")) as fp:
-    json_obj = json.load(fp)
-
-print("Pull Request from event.json:", json_obj["number"])
-
-sys.exit(0)
+sys.exit(1)
