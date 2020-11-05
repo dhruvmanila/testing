554f6986bcaae978792cf466972ad864ec24b8b4
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-05 14:55:37 +0530

    Add doctest

--- check_pr_files.py
+++ check_pr_files.py
@@ -1,12 +1,16 @@
 def test1(abc, bbc, cbc):
 	"""
 	A test function
+	>>> test1()
+	False
 	"""
 	return False
 
 def test2(deaf, eef, fef):
 	"""
 	A test function
+	>>> test2()
+	None
 	"""
 	return None
 
@@ -21,6 +25,10 @@ class TClass:
 		self.test = test
 	
 	def add(self, aab, bab):
+		"""
+		>>> add(5, 10)
+		15
+		"""
 		return a + b
 
 if __name__ == '__main__':
