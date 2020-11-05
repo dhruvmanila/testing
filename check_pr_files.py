a3b19dc9a21d64162d7a39913991ecc5639e5d13
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-05 14:52:27 +0530

    Descriptive names

--- check_pr_files.py
+++ check_pr_files.py
@@ -1,26 +1,26 @@
-def test1(a, b, c):
+def test1(abc, bbc, cbc):
 	"""
 	A test function
 	"""
 	return False
 
-def test2(d, e, f):
+def test2(deaf, eef, fef):
 	"""
 	A test function
 	"""
 	return None
 
-def test3(a: int) -> None:
+def test3(ant: int) -> None:
 	"""
 	>>> findIslands(1, 1, 1)
 	"""
 	return None
 
-class T:
+class TClass:
 	def __init__(self, test):
 		self.test = test
 	
-	def add(self, a, b):
+	def add(self, aab, bab):
 		return a + b
 
 if __name__ == '__main__':
