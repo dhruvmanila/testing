8bfc4f5e77dc8c252346783f436c54a826c9868e
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-06 13:08:33 +0530

    Update check_pr_files.py

--- check_pr_files.py
+++ check_pr_files.py
@@ -1,20 +1,31 @@
-def test1(a, b, c):
+def test1(a):
 	"""
 	A test function
 	"""
 	return False
 
-def test2(d, e, f):
+def test2(d):
 	"""
 	A test function
 	"""
 	return None
 
-def test3(a: int) -> None:
+def test3(e: int) -> None:
 	"""
-	>>> findIslands(1, 1, 1)
+	>>> test3(1, 1, 1)
 	"""
 	return None
+
+
+class TClass:
+	def __init__(self, test: str) -> None:
+		self.test = test
+
+	def add(self, a: int, b: int) -> int:
+		"""
+		Add two numbers
+		"""
+		return a + b
 
 if __name__ == '__main__':
 	pass
