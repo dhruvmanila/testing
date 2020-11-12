3127fe59658f9cf61d0f65394ae414d91bf30b29
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-12 17:55:47 +0530

    Update check_run.py

--- check_run.py
+++ check_run.py
@@ -1,9 +1,3 @@
 import sys
-import os
-import json
-
-print(os.getenv("GITHUB_REF"))
-print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
-print(os.getenv("GITHUB_EVENT_PATH"))
 
 sys.exit(0)
