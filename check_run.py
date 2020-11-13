896afc6ccca78596c2cf829d44c8543352c0ce89
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-13 15:38:54 +0530

    Update check_run.py

--- check_run.py
+++ check_run.py
@@ -1,7 +1,6 @@
 import sys
 import os
 import json
-from pprint import pprint
 
 print(os.getenv("GITHUB_REF"))
 print(os.getenv("GITHUB_EVENT_PATH"))
@@ -12,12 +11,18 @@ path = os.getenv("GITHUB_PATH")
 print(f"{env = }")
 print(f"{path = }")
 
-with open(env) as f:
-  for line in f:
-    print(line)
+_DELIMITER = "EOF"
 
-with open(path) as f:
-  for line in f:
-    print(line)
 
+def setenv(name: str, value: str) -> None:
+    """Creates or updates an environment variable for any actions running next in a
+    job."""
+    file_path = os.environ.get("GITHUB_ENV", "")
+    cmd_value = f"{name}<<{_DELIMITER}{os.linesep}{value}{os.linesep}{_DELIMITER}"
+    print(cmd_value)
+    with open(file_path, "a") as file_handle:
+        file_handle.write(cmd_value + os.linesep)
+
+
+setenv("HELLO", "WORLD")
 sys.exit(0)
