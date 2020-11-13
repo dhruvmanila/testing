e10cc9f7060e7d9df066b008df6a68aa73567638
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-13 15:10:26 +0530

    Update check_run.py

--- check_run.py
+++ check_run.py
@@ -1,9 +1,23 @@
 import sys
 import os
 import json
+from pprint import pprint
 
 print(os.getenv("GITHUB_REF"))
-print("Pull request:", os.getenv("GITHUB_REF").split("/")[2])
 print(os.getenv("GITHUB_EVENT_PATH"))
+
+env = os.getenv("GITHUB_ENV")
+path = os.getenv("GITHUB_PATH")
+
+print(f"{env = }")
+print(f"{path = }")
+
+with open(env) as f:
+  for line in f:
+    print(line)
+
+with open(path) as f:
+  for line in f:
+    print(line)
 
 sys.exit(0)
