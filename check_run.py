32808d03e6601deddf4142c46d9ca7d8f405612e
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-11-29 22:24:29 +0530

    Update check_run.py

--- check_run.py
+++ check_run.py
@@ -12,7 +12,7 @@ print("Event path:", event_path)
 with open(event_path) as f:
   event = json.load(f)
 
-print(event["url"] + "/files")
+print(event["pull_request"]["url"] + "/files")
 pprint(event["number"])
 print(os.getcwd())
   
