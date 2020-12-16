9b51437cfbbe33fbb8e5f8b4712c7a6ff8d326e6
Dhruv Manilawala <dhruvmanila@gmail.com>
2020-12-16 12:17:51 +0530

    Update fixit_framework.py

--- fixit_framework.py
+++ fixit_framework.py
@@ -1,31 +1,31 @@
 def foo(a):
     """
-	  A test function
-	  """
-	  return False
+    A test function
+    """
+    return False
 
 
 def bar(e: int) -> None:
-	  """
-	  >>> bar(1, 1, 1)
-	  """
-	  return None
+    """
+    >>> bar(1, 1, 1)
+    """
+    return None
 
 
 class Spam:
-	  def __init__(self, test: str) -> None:
-		  self.test = test
+    def __init__(self, test: str) -> None:
+        self.test = test
 
     def add(self, a: int, b: int) -> int:
-		    """
-		    Add two numbers
-  		  """
-	  	  return a + b
+        """
+        Add two numbers
+        """
+        return a + b
 
 
 def baz():
     pass
-    
 
-if __name__ == '__main__':
-	  pass
+
+if __name__ == "__main__":
+    pass
